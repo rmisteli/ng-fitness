7fdb38ea44513348b19ee8625135f946a9203656
Reto Misteli <info@rmisteli.ch>
2021-12-29 14:54:23 +0100

    31. Adding Input with Hints and Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 import {AppComponent} from './app.component';
 import {MaterialModule} from "./material.module";
@@ -28,6 +30,8 @@ import {AppRoutingModule} from "./app-routing.module";
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
