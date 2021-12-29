3a145ea89d61c9ac7211c9c552789df6c7e97dda
Reto Misteli <info@rmisteli.ch>
2021-12-29 15:46:18 +0100

    35. Added Checkbox, disabled button for invalid forms

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from "@angular/material/core";
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import {MatNativeDateModule} from "@angular/material/core";
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatCheckboxModule,
   ],
   exports: [
     MatButtonModule,
@@ -23,6 +25,7 @@ import {MatNativeDateModule} from "@angular/material/core";
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatCheckboxModule,
   ]
 })
 export class MaterialModule { }
