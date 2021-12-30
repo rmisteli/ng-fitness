22fcc7703b0ac442225cebb30c7ec4c922ac5205
Reto Misteli <info@rmisteli.ch>
2021-12-30 07:10:45 +0100

    55. Adding new and current training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {HeaderComponent} from './navigation/header/header.component';
 import {SidenavListComponent} from './navigation/sidenav-list/sidenav-list.component';
+import {StopTrainingComponent} from "./training/current-training/stop-training.component";
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import {SidenavListComponent} from './navigation/sidenav-list/sidenav-list.compo
     PastTrainingsComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent
+    SidenavListComponent,
+    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-new-training',
@@ -6,10 +6,22 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
+  @Output() trainingStart = new EventEmitter<void>();
+
+  exercises: {value: string, viewValue: string}[] = [
+    {value:'crunches', viewValue:'Crunches'},
+    {value:'touch-toes', viewValue:'Touch Toes'},
+    {value:'side-lunges', viewValue:'Side Lunges'},
+    {value:'burpees', viewValue:'Burpees'},
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onStartTraining() {
+    this.trainingStart.emit();
   }
 
 }
